e0cd3ab8e4cb82dd67214637682ea73f4c207612
malka3949 <ms0534113949@gmail.com>
2025-06-09 21:14:11 +0300

    first

---
+++ I_Dall/Customer.cs
@@ -0,0 +1,14 @@
+﻿using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Dall
+{
+    public interface  Customer:IDalRepository<T> {
+        public Task<DtoCustomer> SelectByIdAsync(string id);
+        public Task<bool> AddAsync(DtoCustomer customer);   
+    }
+}
